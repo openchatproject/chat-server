ad39e3adefb7583fe6c16c3e9ebb85d22bfe328f
captionkirk <captionkirk@outlook.com>
2018-08-23 22:12:33 +0800

    Fix StoredMessages calls (now all api calls have at least been tested on a running server...)

--- src/main/java/com/openchat/secureim/storage/StoredMessageManager.java
+++ src/main/java/com/openchat/secureim/storage/StoredMessageManager.java
@@ -2,13 +2,20 @@ package com.openchat.secureim.storage;
 
 import com.openchat.secureim.entities.EncryptedOutgoingMessage;
 
+import java.io.IOException;
+import java.util.List;
+
 public class StoredMessageManager {
   StoredMessages storedMessages;
   public StoredMessageManager(StoredMessages storedMessages) {
     this.storedMessages = storedMessages;
   }
 
-  public void storeMessage(Account account, EncryptedOutgoingMessage outgoingMessage) {
-    storedMessages.insert(account.getId(), outgoingMessage);
+  public void storeMessage(Account account, EncryptedOutgoingMessage outgoingMessage) throws IOException {
+    storedMessages.insert(account.getId(), outgoingMessage.serialize());
+  }
+
+  public List<String> getStoredMessage(Account account) {
+    return storedMessages.getMessagesForAccountId(account.getId());
   }
 }
