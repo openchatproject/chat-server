5524bbd58a14ae21ebc93f2c2c81fa08b20c6476
captionkirk <captionkirk@outlook.com>
2018-10-09 22:33:18 +0800

    Use transfer acceleration

--- src/main/java/com/openchat/secureim/util/UrlSigner.java
+++ src/main/java/com/openchat/secureim/util/UrlSigner.java
@@ -5,6 +5,7 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.S3ClientOptions;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.openchat.secureim.configuration.S3Configuration;
 
@@ -26,9 +27,11 @@ public class UrlSigner {
   public URL getPreSignedUrl(long attachmentId, HttpMethod method) {
     AmazonS3                    client  = new AmazonS3Client(credentials);
     GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(bucket, String.valueOf(attachmentId), method);
-
+    
     request.setExpiration(new Date(System.currentTimeMillis() + DURATION));
     request.setContentType("application/octet-stream");
+
+    client.setS3ClientOptions(S3ClientOptions.builder().setAccelerateModeEnabled(true).build());
 
     return client.generatePresignedUrl(request);
   }
