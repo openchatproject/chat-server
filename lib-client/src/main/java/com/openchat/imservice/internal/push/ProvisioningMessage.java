8f5437104f9ea78c97aa1145635f1f9e4c5a83a7
captionkirk <captionkirk@outlook.com>
2018-11-07 12:26:33 +0800

    Fix provisioning flow

--- lib-client/src/main/java/com/openchat/imservice/internal/push/ProvisioningMessage.java
+++ lib-client/src/main/java/com/openchat/imservice/internal/push/ProvisioningMessage.java
@@ -1,7 +1,10 @@
 package com.openchat.imservice.internal.push;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ProvisioningMessage {
 
+  @JsonProperty
   private String body;
 
   public ProvisioningMessage(String body) {
