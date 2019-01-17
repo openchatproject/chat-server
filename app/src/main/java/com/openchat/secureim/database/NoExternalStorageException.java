440a126949f2376e69ff45bd3c9b3d2070e80d2f
TyroneFrancis <tyrone.fran@hotmail.com>
2019-01-17 19:08:49 +0800

    Use minSdkVersion 9

---
+++ app/src/main/java/com/openchat/secureim/database/NoExternalStorageException.java
@@ -0,0 +1,24 @@
+package com.openchat.secureim.database;
+
+public class NoExternalStorageException extends Exception {
+
+  public NoExternalStorageException() {
+    // TODO Auto-generated constructor stub
+  }
+
+  public NoExternalStorageException(String detailMessage) {
+    super(detailMessage);
+    // TODO Auto-generated constructor stub
+  }
+
+  public NoExternalStorageException(Throwable throwable) {
+    super(throwable);
+    // TODO Auto-generated constructor stub
+  }
+
+  public NoExternalStorageException(String detailMessage, Throwable throwable) {
+    super(detailMessage, throwable);
+    // TODO Auto-generated constructor stub
+  }
+
+}
---
+++ app/src/main/java/com/openchat/secureim/database/model/MmsMessageRecord.java
@@ -0,0 +1,56 @@
+package com.openchat.secureim.database.model;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.openchat.secureim.database.documents.IdentityKeyMismatch;
+import com.openchat.secureim.database.documents.NetworkFailure;
+import com.openchat.secureim.mms.Slide;
+import com.openchat.secureim.mms.SlideDeck;
+import com.openchat.secureim.recipients.Recipient;
+
+import java.util.List;
+
+public abstract class MmsMessageRecord extends MessageRecord {
+
+  private final @NonNull SlideDeck slideDeck;
+
+  MmsMessageRecord(Context context, long id, Body body, Recipient conversationRecipient,
+                   Recipient individualRecipient, int recipientDeviceId, long dateSent,
+                   long dateReceived, long threadId, int deliveryStatus, int deliveryReceiptCount,
+                   long type, List<IdentityKeyMismatch> mismatches,
+                   List<NetworkFailure> networkFailures, int subscriptionId, long expiresIn,
+                   long expireStarted, @NonNull SlideDeck slideDeck, int readReceiptCount)
+  {
+    super(context, id, body, conversationRecipient, individualRecipient, recipientDeviceId, dateSent, dateReceived, threadId, deliveryStatus, deliveryReceiptCount, type, mismatches, networkFailures, subscriptionId, expiresIn, expireStarted, readReceiptCount);
+    this.slideDeck = slideDeck;
+  }
+
+  @Override
+  public boolean isMms() {
+    return true;
+  }
+
+  @NonNull
+  public SlideDeck getSlideDeck() {
+    return slideDeck;
+  }
+
+  @Override
+  public boolean isMediaPending() {
+    for (Slide slide : getSlideDeck().getSlides()) {
+      if (slide.isInProgress() || slide.isPendingDownload()) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public boolean containsMediaSlide() {
+    return slideDeck.containsMediaSlide();
+  }
+
+
+}
