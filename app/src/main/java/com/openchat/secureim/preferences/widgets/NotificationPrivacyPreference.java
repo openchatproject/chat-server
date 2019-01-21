1fb8e8db0815615ea2ca4a1310404dcae364d57e
heypat <patrickhedy@hotmail.com>
2019-01-21 10:07:27 +0800

    Fix group migration.

---
+++ app/src/main/java/com/openchat/secureim/preferences/widgets/NotificationPrivacyPreference.java
@@ -0,0 +1,19 @@
+package com.openchat.secureim.preferences.widgets;
+
+public class NotificationPrivacyPreference {
+
+  private final String preference;
+
+  public NotificationPrivacyPreference(String preference) {
+    this.preference = preference;
+  }
+
+  public boolean isDisplayContact() {
+    return "all".equals(preference) || "contact".equals(preference);
+  }
+
+  public boolean isDisplayMessage() {
+    return "all".equals(preference);
+  }
+
+}
