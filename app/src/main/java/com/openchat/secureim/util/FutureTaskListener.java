7c6447c1675ad8e43994d00912a3183d46ed8787
Sam-Lucius <sa.luci@hotmail.com>
2019-01-22 15:31:54 +0800

    All push groups are 'secure' ConversationActivities.

---
+++ app/src/main/java/com/openchat/secureim/util/FutureTaskListener.java
@@ -0,0 +1,8 @@
+package com.openchat.secureim.util;
+
+import java.util.concurrent.ExecutionException;
+
+public interface FutureTaskListener<V> {
+  public void onSuccess(V result);
+  public void onFailure(ExecutionException exception);
+}
