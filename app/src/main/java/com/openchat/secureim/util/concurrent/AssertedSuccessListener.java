fcb256d7508fb3b7d81dfb51b7e7420af77632fe
heypat <patrickhedy@hotmail.com>
2019-01-23 10:07:34 +0800

    Putting this functionality in the hands of the user was a bad idea.

---
+++ app/src/main/java/com/openchat/secureim/util/concurrent/AssertedSuccessListener.java
@@ -0,0 +1,12 @@
+package com.openchat.secureim.util.concurrent;
+
+import com.openchat.secureim.util.concurrent.ListenableFuture.Listener;
+
+import java.util.concurrent.ExecutionException;
+
+public abstract class AssertedSuccessListener<T> implements Listener<T> {
+  @Override
+  public void onFailure(ExecutionException e) {
+    throw new AssertionError(e);
+  }
+}
