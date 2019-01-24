0613f5952a74ac509a97370006f070397930b365
TyroneFrancis <tyrone.fran@hotmail.com>
2019-01-24 16:49:30 +0800

    re-privatize getRecipientForNumber

---
+++ app/src/main/java/com/openchat/secureim/sms/IncomingIdentityDefaultMessage.java
@@ -0,0 +1,15 @@
+package com.openchat.secureim.sms;
+
+
+public class IncomingIdentityDefaultMessage extends IncomingTextMessage {
+
+  public IncomingIdentityDefaultMessage(IncomingTextMessage base) {
+    super(base, "");
+  }
+
+  @Override
+  public boolean isIdentityDefault() {
+    return true;
+  }
+
+}
