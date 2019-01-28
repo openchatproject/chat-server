1b6d1934ab724f4272b048624a4db4e801552d1d
heypat <patrickhedy@hotmail.com>
2019-01-28 10:39:01 +0800

    Fix for identity key mismatch on devices with V1 identities.

---
+++ app/src/main/java/com/openchat/secureim/mms/MediaTooLargeException.java
@@ -0,0 +1,24 @@
+package com.openchat.secureim.mms;
+
+public class MediaTooLargeException extends Exception {
+
+  public MediaTooLargeException() {
+    // TODO Auto-generated constructor stub
+  }
+
+  public MediaTooLargeException(String detailMessage) {
+    super(detailMessage);
+    // TODO Auto-generated constructor stub
+  }
+
+  public MediaTooLargeException(Throwable throwable) {
+    super(throwable);
+    // TODO Auto-generated constructor stub
+  }
+
+  public MediaTooLargeException(String detailMessage, Throwable throwable) {
+    super(detailMessage, throwable);
+    // TODO Auto-generated constructor stub
+  }
+
+}
