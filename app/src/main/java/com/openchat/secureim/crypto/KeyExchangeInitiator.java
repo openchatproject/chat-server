74f5d3e0aa421f26360c498384cdc8ba005b4ce3
TyroneFrancis <tyrone.fran@hotmail.com>
2019-02-27 15:04:16 +0800

    SessionBuilder improvements, more extensive SessionBuilder tests.

--- app/src/main/java/com/openchat/secureim/crypto/KeyExchangeInitiator.java
+++ app/src/main/java/com/openchat/secureim/crypto/KeyExchangeInitiator.java
@@ -9,19 +9,17 @@ import com.openchat.secureim.recipients.Recipient;
 import com.openchat.secureim.sms.MessageSender;
 import com.openchat.secureim.sms.OutgoingKeyExchangeMessage;
 import com.openchat.secureim.util.Dialogs;
-import com.openchat.protocal.IdentityKeyPair;
-import com.openchat.protocal.ecc.Curve;
-import com.openchat.protocal.ecc.ECKeyPair;
+import com.openchat.protocal.SessionBuilder;
 import com.openchat.protocal.protocol.KeyExchangeMessage;
+import com.openchat.protocal.state.IdentityKeyStore;
+import com.openchat.protocal.state.PreKeyStore;
 import com.openchat.protocal.state.SessionRecord;
 import com.openchat.protocal.state.SessionStore;
 import com.openchat.imservice.crypto.MasterSecret;
 import com.openchat.imservice.storage.RecipientDevice;
+import com.openchat.imservice.storage.OpenchatServicePreKeyStore;
 import com.openchat.imservice.storage.OpenchatServiceSessionStore;
 import com.openchat.imservice.util.Base64;
-
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 
 public class KeyExchangeInitiator {
 
@@ -45,23 +43,17 @@ public class KeyExchangeInitiator {
   }
 
   private static void initiateKeyExchange(Context context, MasterSecret masterSecret, Recipient recipient) {
-    int             sequence     = getRandomSequence();
-    int             flags        = KeyExchangeMessage.INITIATE_FLAG;
-    ECKeyPair       baseKey      = Curve.generateKeyPair(true);
-    ECKeyPair       ephemeralKey = Curve.generateKeyPair(true);
-    IdentityKeyPair identityKey  = IdentityKeyUtil.getIdentityKeyPair(context, masterSecret);
+    SessionStore     sessionStore     = new OpenchatServiceSessionStore(context, masterSecret);
+    PreKeyStore      preKeyStore      = new OpenchatServicePreKeyStore(context, masterSecret);
+    IdentityKeyStore identityKeyStore = new OpenchatServiceIdentityKeyStore(context, masterSecret);
 
-    KeyExchangeMessage message = new KeyExchangeMessage(sequence, flags,
-                                                        baseKey.getPublicKey(),
-                                                        ephemeralKey.getPublicKey(),
-                                                        identityKey.getPublicKey());
+    SessionBuilder   sessionBuilder   = new SessionBuilder(sessionStore, preKeyStore, identityKeyStore,
+                                                           recipient.getRecipientId(),
+                                                           RecipientDevice.DEFAULT_DEVICE_ID);
 
-    OutgoingKeyExchangeMessage textMessage     = new OutgoingKeyExchangeMessage(recipient, Base64.encodeBytesWithoutPadding(message.serialize()));
-    SessionStore               sessionStore    = new OpenchatServiceSessionStore(context, masterSecret);
-    SessionRecord              sessionRecord   = sessionStore.get(recipient.getRecipientId(), RecipientDevice.DEFAULT_DEVICE_ID);
-
-    sessionRecord.getSessionState().setPendingKeyExchange(sequence, baseKey, ephemeralKey, identityKey);
-    sessionStore.put(recipient.getRecipientId(), RecipientDevice.DEFAULT_DEVICE_ID, sessionRecord);
+    KeyExchangeMessage         keyExchangeMessage = sessionBuilder.process();
+    String                     serializedMessage  = Base64.encodeBytesWithoutPadding(keyExchangeMessage.serialize());
+    OutgoingKeyExchangeMessage textMessage        = new OutgoingKeyExchangeMessage(recipient, serializedMessage);
 
     MessageSender.send(context, masterSecret, textMessage, -1, false);
   }
@@ -73,16 +65,5 @@ public class KeyExchangeInitiator {
     SessionRecord sessionRecord = sessionStore.get(recipient.getRecipientId(), RecipientDevice.DEFAULT_DEVICE_ID);
 
     return sessionRecord.getSessionState().hasPendingPreKey();
-  }
-
-  private static int getRandomSequence() {
-    try {
-      SecureRandom random    = SecureRandom.getInstance("SHA1PRNG");
-      int          candidate = Math.abs(random.nextInt());
-
-      return candidate % 65535;
-    } catch (NoSuchAlgorithmException e) {
-      throw new AssertionError(e);
-    }
   }
 }
