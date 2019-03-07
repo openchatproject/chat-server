93553c08076c72363502a0d4a99ad0d491a2d3f5
heypat <patrickhedy@hotmail.com>
2019-03-07 17:16:29 +0800

    Short circuit self-send.

--- app/src/main/java/com/openchat/secureim/sms/MessageSender.java
+++ app/src/main/java/com/openchat/secureim/sms/MessageSender.java
@@ -3,15 +3,27 @@ package com.openchat.secureim.sms;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.util.Pair;
 
+import com.openchat.secureim.database.EncryptingSmsDatabase;
+import com.openchat.secureim.database.MmsDatabase;
+import com.openchat.secureim.database.MmsSmsColumns;
+import com.openchat.secureim.mms.IncomingMediaMessage;
 import com.openchat.secureim.mms.OutgoingMediaMessage;
+import com.openchat.secureim.recipients.Recipients;
+import com.openchat.secureim.util.OpenchatServicePreferences;
+import com.openchat.secureim.util.Util;
 import com.openchat.imservice.crypto.MasterSecret;
 import com.openchat.secureim.database.DatabaseFactory;
 import com.openchat.secureim.service.SendReceiveService;
+import com.openchat.imservice.push.IncomingPushMessage;
+import com.openchat.imservice.push.PushMessageProtos;
+import com.openchat.imservice.util.InvalidNumberException;
 
 import java.util.List;
 
 import ws.com.google.android.mms.MmsException;
+import ws.com.google.android.mms.pdu.RetrieveConf;
 
 public class MessageSender {
 
@@ -19,19 +31,31 @@ public class MessageSender {
                           OutgoingTextMessage message, long threadId,
                           boolean forceSms)
   {
-    if (threadId == -1)
+    EncryptingSmsDatabase database = DatabaseFactory.getEncryptingSmsDatabase(context);
+
+    if (threadId == -1) {
       threadId = DatabaseFactory.getThreadDatabase(context).getThreadIdFor(message.getRecipients());
+    }
 
-    List<Long> messageIds = DatabaseFactory.getEncryptingSmsDatabase(context)
-        .insertMessageOutbox(masterSecret, threadId, message, forceSms);
+    List<Long> messageIds = database.insertMessageOutbox(masterSecret, threadId, message, forceSms);
 
-    for (long messageId : messageIds) {
-      Log.w("SMSSender", "Got message id for new message: " + messageId);
+    if (!forceSms && isSelfSend(context, message.getRecipients())) {
+      for (long messageId : messageIds) {
+        database.markAsSent(messageId);
+        database.markAsPush(messageId);
 
-      Intent intent = new Intent(SendReceiveService.SEND_SMS_ACTION, null,
-                                 context, SendReceiveService.class);
-      intent.putExtra("message_id", messageId);
-      context.startService(intent);
+        Pair<Long, Long> messageAndThreadId = database.copyMessageInbox(messageId);
+        database.markAsPush(messageAndThreadId.first);
+      }
+    } else {
+      for (long messageId : messageIds) {
+        Log.w("SMSSender", "Got message id for new message: " + messageId);
+
+        Intent intent = new Intent(SendReceiveService.SEND_SMS_ACTION, null,
+                                   context, SendReceiveService.class);
+        intent.putExtra("message_id", messageId);
+        context.startService(intent);
+      }
     }
 
     return threadId;
@@ -42,18 +66,27 @@ public class MessageSender {
                           long threadId, boolean forceSms)
       throws MmsException
   {
-    if (threadId == -1)
+    MmsDatabase database = DatabaseFactory.getMmsDatabase(context);
+
+    if (threadId == -1) {
       threadId = DatabaseFactory.getThreadDatabase(context).getThreadIdFor(message.getRecipients(), message.getDistributionType());
+    }
 
-    long messageId = DatabaseFactory.getMmsDatabase(context)
-                                    .insertMessageOutbox(masterSecret, message, threadId, forceSms);
+    long messageId = database.insertMessageOutbox(masterSecret, message, threadId, forceSms);
 
-    Intent intent  = new Intent(SendReceiveService.SEND_MMS_ACTION, null,
-                                context, SendReceiveService.class);
-    intent.putExtra("message_id", messageId);
-    intent.putExtra("thread_id", threadId);
+    if (!forceSms && isSelfSend(context, message.getRecipients())) {
+      database.markAsSent(messageId, "self-send".getBytes(), 0);
+      database.markAsPush(messageId);
+      long newMessageId = database.copyMessageInbox(masterSecret, messageId);
+      database.markAsPush(newMessageId);
+    } else {
+      Intent intent  = new Intent(SendReceiveService.SEND_MMS_ACTION, null,
+                                  context, SendReceiveService.class);
+      intent.putExtra("message_id", messageId);
+      intent.putExtra("thread_id", threadId);
 
-    context.startService(intent);
+      context.startService(intent);
+    }
 
     return threadId;
   }
@@ -73,6 +106,24 @@ public class MessageSender {
     }
     intent.putExtra("message_id", messageId);
     context.startService(intent);
+  }
+
+  private static boolean isSelfSend(Context context, Recipients recipients) {
+    try {
+      if (!OpenchatServicePreferences.isPushRegistered(context)) {
+        return false;
+      }
+
+      if (!recipients.isSingleRecipient()) {
+        return false;
+      }
+
+      String e164number = Util.canonicalizeNumber(context, recipients.getPrimaryRecipient().getNumber());
+      return OpenchatServicePreferences.getLocalNumber(context).equals(e164number);
+    } catch (InvalidNumberException e) {
+      Log.w("MessageSender", e);
+      return false;
+    }
   }
 
 }
