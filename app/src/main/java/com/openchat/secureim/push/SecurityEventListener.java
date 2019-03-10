da516fdb7823ee41776d783b1270bedb4d1787b0
TyroneFrancis <tyrone.fran@hotmail.com>
2019-03-10 22:20:18 +0800

    Fix some bugs with PKWM padding and attachment detection.

--- app/src/main/java/com/openchat/secureim/push/SecurityEventListener.java
+++ app/src/main/java/com/openchat/secureim/push/SecurityEventListener.java
@@ -2,15 +2,16 @@ package com.openchat.secureim.push;
 
 import android.content.Context;
 
-import com.openchat.secureim.ApplicationContext;
 import com.openchat.secureim.crypto.SecurityEvent;
 import com.openchat.secureim.database.DatabaseFactory;
-import com.openchat.secureim.jobs.RefreshPreKeysJob;
 import com.openchat.secureim.recipients.RecipientFactory;
 import com.openchat.secureim.recipients.Recipients;
 import com.openchat.imservice.api.OpenchatServiceMessageSender;
 
 public class SecurityEventListener implements OpenchatServiceMessageSender.EventListener {
+
+  private static final String TAG = SecurityEventListener.class.getSimpleName();
+
   private final Context context;
 
   public SecurityEventListener(Context context) {
@@ -23,10 +24,6 @@ public class SecurityEventListener implements OpenchatServiceMessageSender.Event
     long       threadId   = DatabaseFactory.getThreadDatabase(context).getThreadIdFor(recipients);
 
     SecurityEvent.broadcastSecurityUpdateEvent(context, threadId);
-
-    ApplicationContext.getInstance(context)
-                      .getJobManager()
-                      .add(new RefreshPreKeysJob(context));
   }
 
 }
