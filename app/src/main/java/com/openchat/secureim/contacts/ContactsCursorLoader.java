223cd13b12a8e24d51b61b56e3a16a81e55bcf62
Sam-Lucius <sa.luci@hotmail.com>
2019-03-31 21:29:04 +0800

    refresh contacts database on new db query

--- app/src/main/java/com/openchat/secureim/contacts/ContactsCursorLoader.java
+++ app/src/main/java/com/openchat/secureim/contacts/ContactsCursorLoader.java
@@ -20,6 +20,7 @@ public class ContactsCursorLoader extends CursorLoader {
 
   @Override
   public Cursor loadInBackground() {
+    ContactsDatabase.destroyInstance();
     db = ContactsDatabase.getInstance(context);
     return db.query(filter, pushOnly);
   }
