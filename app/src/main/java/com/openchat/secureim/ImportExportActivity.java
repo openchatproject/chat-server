04e66bba9eb2ba3463f67bc168a1b1f3a1035f6e
TyroneFrancis <tyrone.fran@hotmail.com>
2019-04-01 10:57:55 +0800

    Revert "add theme to import/export activity"

--- app/src/main/java/com/openchat/secureim/ImportExportActivity.java
+++ app/src/main/java/com/openchat/secureim/ImportExportActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
 
 import com.openchat.secureim.crypto.MasterSecret;
-import com.openchat.secureim.util.DynamicTheme;
 
 public class ImportExportActivity extends PassphraseRequiredActionBarActivity {
 
@@ -18,24 +17,15 @@ public class ImportExportActivity extends PassphraseRequiredActionBarActivity {
   private ViewPager viewPager;
   private MasterSecret masterSecret;
 
-  private DynamicTheme dynamicTheme = new DynamicTheme();
-
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    dynamicTheme.onCreate(this);
     setContentView(R.layout.import_export_activity);
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     initializeResources();
     initializeViewPager();
     initializeTabs();
-  }
-
-  @Override
-  public void onResume() {
-      super.onResume();
-      dynamicTheme.onResume(this);
   }
 
   @Override
