e5e3519b6e7cda5f6d6c42f206352070b517bcc1
captionkirk <captionkirk@outlook.com>
2019-04-07 20:06:58 +0800

    replace icons in the add attachment dialog

--- app/src/main/java/com/openchat/secureim/ConversationActivity.java
+++ app/src/main/java/com/openchat/secureim/ConversationActivity.java
@@ -536,7 +536,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   private void handleAddAttachment() {
     if (this.isMmsEnabled || DirectoryHelper.isPushDestination(this, getRecipients())) {
       AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.OpenchatService_Light_Dialog));
-      builder.setIcon(R.drawable.ic_dialog_attach);
+      builder.setIcon(R.drawable.ic_attach_file);
       builder.setTitle(R.string.ConversationActivity_add_attachment);
       builder.setAdapter(attachmentAdapter, new AttachmentTypeListener());
       builder.show();
