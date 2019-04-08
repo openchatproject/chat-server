aa60f5f1e854afafdcf1607ac5998b4f32b5e19c
captionkirk <captionkirk@outlook.com>
2019-04-08 12:03:52 +0800

    fix MessageDetailsActivity home button

--- app/src/main/java/com/openchat/secureim/MessageDetailsActivity.java
+++ app/src/main/java/com/openchat/secureim/MessageDetailsActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.Loader;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
@@ -64,6 +65,7 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
 
     initializeResources();
 
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     getSupportLoaderManager().initLoader(0, null, this);
   }
 
@@ -180,6 +182,17 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
   @Override
   public void onLoaderReset(Loader<Cursor> loader) {
     recipientsList.setAdapter(null);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    super.onOptionsItemSelected(item);
+
+    switch (item.getItemId()) {
+      case android.R.id.home: finish(); return true;
+    }
+
+    return false;
   }
 
   private class MessageRecipientAsyncTask extends AsyncTask<Void,Void,Recipients> {
