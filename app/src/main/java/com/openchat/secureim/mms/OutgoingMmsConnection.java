02870dbf06516746aa7924179cc23dfbe311d28d
Sam-Lucius <sa.luci@hotmail.com>
2019-04-16 17:24:56 +0800

    add @Nullable annotation to outoging mms api

--- app/src/main/java/com/openchat/secureim/mms/OutgoingMmsConnection.java
+++ app/src/main/java/com/openchat/secureim/mms/OutgoingMmsConnection.java
@@ -1,11 +1,12 @@
 package com.openchat.secureim.mms;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.openchat.secureim.transport.UndeliverableMessageException;
 
 import ws.com.google.android.mms.pdu.SendConf;
 
 public interface OutgoingMmsConnection {
-  SendConf send(@NonNull byte[] pduBytes) throws UndeliverableMessageException;
+  @Nullable SendConf send(@NonNull byte[] pduBytes) throws UndeliverableMessageException;
 }
