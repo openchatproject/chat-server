2331d5cb04fe8b58b3c6ef6481c9b1b6db320330
Sam-Lucius <sa.luci@hotmail.com>
2019-04-18 13:45:05 +0800

    Prompt user to rate app

--- app/src/main/java/com/openchat/secureim/ConversationListActivity.java
+++ app/src/main/java/com/openchat/secureim/ConversationListActivity.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.openchat.secureim.components.RatingManager;
 import com.openchat.secureim.crypto.MasterSecret;
 import com.openchat.secureim.database.DatabaseFactory;
 import com.openchat.secureim.notifications.MessageNotifier;
@@ -52,6 +53,7 @@ public class ConversationListActivity extends PassphraseRequiredActionBarActivit
     initializeContactUpdatesReceiver();
 
     DirectoryRefreshListener.schedule(this);
+    RatingManager.showRatingDialogIfNecessary(this);
   }
 
   @Override
