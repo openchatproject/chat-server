8bf23f98465068891b46b4832e98c7fd8722eed0
Sam-Lucius <sa.luci@hotmail.com>
2019-04-21 12:05:08 +0800

    enforce NonNull on incoming MMS content location

--- app/src/main/java/com/openchat/secureim/mms/IncomingLegacyMmsConnection.java
+++ app/src/main/java/com/openchat/secureim/mms/IncomingLegacyMmsConnection.java
@@ -2,6 +2,7 @@ package com.openchat.secureim.mms;
 
 import android.content.Context;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
@@ -42,7 +43,10 @@ public class IncomingLegacyMmsConnection extends LegacyMmsConnection implements 
   }
 
   @Override
-  public @Nullable RetrieveConf retrieve(String contentLocation, byte[] transactionId) throws MmsRadioException, ApnUnavailableException, IOException {
+  public @Nullable RetrieveConf retrieve(@NonNull String contentLocation,
+                                         byte[] transactionId)
+      throws MmsRadioException, ApnUnavailableException, IOException
+  {
     MmsRadio radio = MmsRadio.getInstance(context);
     Apn contentApn = new Apn(contentLocation, apn.getProxy(), Integer.toString(apn.getPort()), apn.getUsername(), apn.getPassword());
     if (isCdmaDevice()) {
