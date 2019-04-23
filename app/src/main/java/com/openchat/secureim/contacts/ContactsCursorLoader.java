67d4c2598cbc91db14bd2653e7a313838c395ebc
TyroneFrancis <tyrone.fran@hotmail.com>
2019-04-23 16:46:27 +0800

    maintain one ContactsDatabase instance per Loader

--- app/src/main/java/com/openchat/secureim/contacts/ContactsCursorLoader.java
+++ app/src/main/java/com/openchat/secureim/contacts/ContactsCursorLoader.java
@@ -3,12 +3,20 @@ package com.openchat.secureim.contacts;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.content.CursorLoader;
+import android.util.Log;
+
+import junit.framework.Assert;
+
+import java.util.concurrent.Semaphore;
 
 public class ContactsCursorLoader extends CursorLoader {
+  private static final String TAG        = ContactsCursorLoader.class.getSimpleName();
+  private static final int    DB_PERMITS = 100;
 
   private final Context          context;
   private final String           filter;
   private final boolean          pushOnly;
+  private final Semaphore        dbSemaphore = new Semaphore(DB_PERMITS);
   private       ContactsDatabase db;
 
   public ContactsCursorLoader(Context context, String filter, boolean pushOnly) {
@@ -16,17 +24,33 @@ public class ContactsCursorLoader extends CursorLoader {
     this.context  = context;
     this.filter   = filter;
     this.pushOnly = pushOnly;
+    this.db       = new ContactsDatabase(context);
   }
 
   @Override
   public Cursor loadInBackground() {
-    ContactsDatabase.destroyInstance();
-    db = ContactsDatabase.getInstance(context);
-    return db.query(filter, pushOnly);
+    try {
+      dbSemaphore.acquire();
+      return db.query(filter, pushOnly);
+    } catch (InterruptedException ie) {
+      throw new AssertionError(ie);
+    } finally {
+      dbSemaphore.release();
+    }
   }
 
   @Override
   public void onReset() {
+    Log.w(TAG, "onReset()");
+    try {
+      dbSemaphore.acquire(DB_PERMITS);
+      db.close();
+      db = new ContactsDatabase(context);
+    } catch (InterruptedException ie) {
+      throw new AssertionError(ie);
+    } finally {
+      dbSemaphore.release(DB_PERMITS);
+    }
     super.onReset();
   }
 }
