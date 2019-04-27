6e753cd7bea69e0a9a19a680aedb0dbe221dd98a
kingstand <mrstanking@hotmail.com>
2019-04-27 16:00:44 +0800

    missing import

--- app/src/main/java/com/openchat/secureim/components/ComposeText.java
+++ app/src/main/java/com/openchat/secureim/components/ComposeText.java
@@ -9,6 +9,8 @@ import android.text.TextUtils;
 import android.text.style.RelativeSizeSpan;
 import android.util.AttributeSet;
 
+import com.openchat.secureim.components.emoji.EmojiEditText;
+
 public class ComposeText extends EmojiEditText {
   public ComposeText(Context context) {
     super(context);
