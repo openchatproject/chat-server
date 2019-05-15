3566d967903971d71e021c90f0db87317d4a79e1
captionkirk <captionkirk@outlook.com>
2019-05-15 14:53:48 +0800

    Support "quick response" when OpenchatService is unlocked.

--- app/src/main/java/com/openchat/secureim/service/QuickResponseService.java
+++ app/src/main/java/com/openchat/secureim/service/QuickResponseService.java
@@ -1,31 +1,62 @@
 package com.openchat.secureim.service;
 
-import android.app.Service;
 import android.content.Intent;
-import android.os.IBinder;
+import android.support.annotation.Nullable;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.openchat.secureim.R;
+import com.openchat.secureim.crypto.MasterSecret;
+import com.openchat.secureim.database.ThreadDatabase;
+import com.openchat.secureim.mms.OutgoingMediaMessage;
+import com.openchat.secureim.mms.SlideDeck;
+import com.openchat.secureim.recipients.RecipientFactory;
+import com.openchat.secureim.recipients.Recipients;
+import com.openchat.secureim.sms.MessageSender;
+import com.openchat.secureim.sms.OutgoingTextMessage;
+import com.openchat.secureim.util.Rfc5724Uri;
 
-public class QuickResponseService extends Service {
+import java.net.URISyntaxException;
 
-  public int onStartCommand(Intent intent, int flags, int startId) {
-    if (!TelephonyManager.ACTION_RESPOND_VIA_MESSAGE.equals(intent.getAction())) {
-      Log.w("QuickResponseService", "Received unknown intent: " + intent.getAction());
-      return START_NOT_STICKY;
-    }
+public class QuickResponseService extends MasterSecretIntentService {
 
-    Toast.makeText(this,
-                   getString(R.string.QuickResponseService_sorry_quick_response_is_not_yet_supported_by_openchatservice),
-                   Toast.LENGTH_LONG).show();
+  private static final String TAG = QuickResponseService.class.getSimpleName();
 
-    return START_NOT_STICKY;
+  public QuickResponseService() {
+    super("QuickResponseService");
   }
 
   @Override
-  public IBinder onBind(Intent intent) {
-    return null;
+  protected void onHandleIntent(Intent intent, @Nullable MasterSecret masterSecret) {
+    if (!TelephonyManager.ACTION_RESPOND_VIA_MESSAGE.equals(intent.getAction())) {
+      Log.w(TAG, "Received unknown intent: " + intent.getAction());
+      return;
+    }
+
+    if (masterSecret == null) {
+      Log.w(TAG, "Got quick response request when locked...");
+      Toast.makeText(this, R.string.QuickResponseService_quick_response_unavailable_when_OpenchatService_is_locked, Toast.LENGTH_LONG).show();
+      return;
+    }
+
+    try {
+      Rfc5724Uri uri        = new Rfc5724Uri(intent.getDataString());
+      String     content    = intent.getStringExtra(Intent.EXTRA_TEXT);
+      Recipients recipients = RecipientFactory.getRecipientsFromString(this, uri.getPath(), false);
+
+      if (!TextUtils.isEmpty(content)) {
+        if (recipients.isSingleRecipient()) {
+          MessageSender.send(this, masterSecret, new OutgoingTextMessage(recipients, content), -1, false);
+        } else {
+          MessageSender.send(this, masterSecret, new OutgoingMediaMessage(this, recipients, new SlideDeck(), content,
+                                                                          ThreadDatabase.DistributionTypes.DEFAULT), -1, false);
+        }
+      }
+    } catch (URISyntaxException e) {
+      Toast.makeText(this, R.string.QuickResponseService_problem_sending_message, Toast.LENGTH_LONG).show();
+      Log.w(TAG, e);
+    }
   }
 }
