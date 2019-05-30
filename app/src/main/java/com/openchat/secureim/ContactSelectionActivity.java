6a7ec8550e13a4136096278e65d31c201b619eea
TyroneFrancis <tyrone.fran@hotmail.com>
2019-05-30 15:28:34 +0800

    Update look of contact selection activities.

---
+++ app/src/main/java/com/openchat/secureim/ContactSelectionActivity.java
@@ -0,0 +1,176 @@
+package com.openchat.secureim;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.openchat.secureim.components.AnimatingToggle;
+import com.openchat.secureim.crypto.MasterSecret;
+import com.openchat.secureim.util.DirectoryHelper;
+import com.openchat.secureim.util.DynamicLanguage;
+import com.openchat.secureim.util.DynamicNoActionBarTheme;
+import com.openchat.secureim.util.DynamicTheme;
+
+import java.io.IOException;
+
+public abstract class ContactSelectionActivity extends PassphraseRequiredActionBarActivity
+                                               implements SwipeRefreshLayout.OnRefreshListener,
+                                                          ContactSelectionListFragment.OnContactSelectedListener
+{
+  private static final String TAG             = ContactSelectionActivity.class.getSimpleName();
+  public  final static String PUSH_ONLY_EXTRA = "push_only";
+
+  private final DynamicTheme    dynamicTheme    = new DynamicNoActionBarTheme();
+  private final DynamicLanguage dynamicLanguage = new DynamicLanguage();
+
+  protected ContactSelectionListFragment contactsFragment;
+
+  private   Toolbar         toolbar;
+  private   EditText        searchText;
+  private   AnimatingToggle toggle;
+  protected ImageView       action;
+  private   ImageView       keyboardToggle;
+  private   ImageView       dialpadToggle;
+  private   ImageView       clearToggle;
+
+  @Override
+  protected void onPreCreate() {
+    dynamicTheme.onCreate(this);
+    dynamicLanguage.onCreate(this);
+  }
+
+  @Override
+  protected void onCreate(Bundle icicle, @NonNull MasterSecret masterSecret) {
+    setContentView(R.layout.contact_selection_activity);
+
+    initializeToolbar();
+    initializeResources();
+    initializeSearch();
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    dynamicTheme.onResume(this);
+    dynamicLanguage.onResume(this);
+  }
+
+  private void initializeToolbar() {
+    this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+    getSupportActionBar().setDisplayShowTitleEnabled(false);
+  }
+
+  private void initializeResources() {
+    this.action         = (ImageView) findViewById(R.id.action_icon);
+    this.searchText     = (EditText) findViewById(R.id.search_view);
+    this.toggle         = (AnimatingToggle) findViewById(R.id.button_toggle);
+    this.keyboardToggle = (ImageView) findViewById(R.id.search_keyboard);
+    this.dialpadToggle  = (ImageView) findViewById(R.id.search_dialpad);
+    this.clearToggle    = (ImageView) findViewById(R.id.search_clear);
+
+    contactsFragment = (ContactSelectionListFragment) getSupportFragmentManager().findFragmentById(R.id.contact_selection_list_fragment);
+    contactsFragment.setOnContactSelectedListener(this);
+    contactsFragment.setOnRefreshListener(this);
+
+    this.keyboardToggle.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        searchText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+        toggle.display(dialpadToggle);
+      }
+    });
+
+    this.dialpadToggle.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        searchText.setInputType(InputType.TYPE_CLASS_PHONE);
+        toggle.display(keyboardToggle);
+      }
+    });
+
+    this.clearToggle.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        searchText.setText("");
+
+        if (SearchUtil.isTextInput(searchText)) toggle.display(dialpadToggle);
+        else                                    toggle.display(keyboardToggle);
+      }
+    });
+  }
+
+  private void initializeSearch() {
+    this.searchText.addTextChangedListener(new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+      }
+
+      @Override
+      public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+      }
+
+      @Override
+      public void afterTextChanged(Editable s) {
+        if      (!SearchUtil.isEmpty(searchText))     toggle.display(clearToggle);
+        else if (SearchUtil.isTextInput(searchText))  toggle.display(dialpadToggle);
+        else if (SearchUtil.isPhoneInput(searchText)) toggle.display(keyboardToggle);
+
+        contactsFragment.setQueryFilter(searchText.getText().toString());
+      }
+    });
+  }
+
+  @Override
+  public void onRefresh() {
+    new AsyncTask<Void, Void, Void>() {
+      @Override
+      protected Void doInBackground(Void... params) {
+        try {
+          DirectoryHelper.refreshDirectory(ContactSelectionActivity.this);
+        } catch (IOException e) {
+          Log.w(TAG, e);
+        }
+
+        return null;
+      }
+
+      @Override
+      protected void onPostExecute(Void result) {
+        searchText.setText("");
+        contactsFragment.resetQueryFilter();
+      }
+    }.execute();
+  }
+
+  @Override
+  public void onContactSelected(String number) {}
+
+  private static class SearchUtil {
+
+    public static boolean isTextInput(EditText editText) {
+      return (editText.getInputType() & 0x0000000F) == InputType.TYPE_CLASS_TEXT;
+    }
+
+    public static boolean isPhoneInput(EditText editText) {
+      return (editText.getInputType() & 0x0000000F) == InputType.TYPE_CLASS_PHONE;
+    }
+
+    public static boolean isEmpty(EditText editText) {
+      return editText.getText().length() <= 0;
+    }
+  }
+}
--- app/src/main/java/com/openchat/secureim/NewConversationActivity.java
+++ app/src/main/java/com/openchat/secureim/NewConversationActivity.java
@@ -1,93 +1,20 @@
 package com.openchat.secureim;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
-import com.openchat.secureim.crypto.MasterSecret;
 import com.openchat.secureim.database.DatabaseFactory;
 import com.openchat.secureim.database.ThreadDatabase;
 import com.openchat.secureim.recipients.RecipientFactory;
 import com.openchat.secureim.recipients.Recipients;
-import com.openchat.secureim.util.DirectoryHelper;
-import com.openchat.secureim.util.DynamicLanguage;
-import com.openchat.secureim.util.DynamicTheme;
-import com.openchat.secureim.util.OpenchatServicePreferences;
 
-public class NewConversationActivity extends PassphraseRequiredActionBarActivity {
+public class NewConversationActivity extends ContactSelectionActivity {
+
   private static final String TAG = NewConversationActivity.class.getSimpleName();
 
-  private final DynamicTheme    dynamicTheme    = new DynamicTheme   ();
-  private final DynamicLanguage dynamicLanguage = new DynamicLanguage();
-
-  private PushContactSelectionListFragment contactsFragment;
-
   @Override
-  protected void onPreCreate() {
-    dynamicTheme.onCreate(this);
-    dynamicLanguage.onCreate(this);
-  }
+  public void onContactSelected(String number) {
+    Recipients recipients = RecipientFactory.getRecipientsFromString(this, number, true);
 
-  @Override
-  protected void onCreate(Bundle icicle, @NonNull MasterSecret masterSecret) {
-    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    setContentView(R.layout.new_conversation_activity);
-    initializeResources();
-  }
-
-  @Override
-  public void onResume() {
-    super.onResume();
-    dynamicTheme.onResume(this);
-    dynamicLanguage.onResume(this);
-    getSupportActionBar().setTitle(R.string.AndroidManifest__select_contacts);
-  }
-
-  @Override
-  public boolean onPrepareOptionsMenu(Menu menu) {
-    MenuInflater inflater = this.getMenuInflater();
-    menu.clear();
-
-    if (OpenchatServicePreferences.isPushRegistered(this)) inflater.inflate(R.menu.push_directory, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    super.onOptionsItemSelected(item);
-    switch (item.getItemId()) {
-    case R.id.menu_refresh_directory:  handleDirectoryRefresh();  return true;
-    case android.R.id.home:            finish();                  return true;
-    }
-    return false;
-  }
-
-  private void initializeResources() {
-    contactsFragment = (PushContactSelectionListFragment) getSupportFragmentManager().findFragmentById(R.id.contact_selection_list_fragment);
-    contactsFragment.setOnContactSelectedListener(new PushContactSelectionListFragment.OnContactSelectedListener() {
-      @Override
-      public void onContactSelected(String number) {
-        Log.i(TAG, "Choosing contact from list.");
-        Recipients recipients = RecipientFactory.getRecipientsFromString(NewConversationActivity.this, number, true);
-        openNewConversation(recipients);
-      }
-    });
-  }
-
-  private void handleDirectoryRefresh() {
-    DirectoryHelper.refreshDirectoryWithProgressDialog(this, new DirectoryHelper.DirectoryUpdateFinishedListener() {
-      @Override
-      public void onUpdateFinished() {
-        contactsFragment.update();
-      }
-    });
-  }
-
-  private void openNewConversation(Recipients recipients) {
     if (recipients != null) {
       Intent intent = new Intent(this, ConversationActivity.class);
       intent.putExtra(ConversationActivity.RECIPIENTS_EXTRA, recipients.getIds());
@@ -95,11 +22,14 @@ public class NewConversationActivity extends PassphraseRequiredActionBarActivity
       intent.putExtra(ConversationActivity.DRAFT_AUDIO_EXTRA, getIntent().getParcelableExtra(ConversationActivity.DRAFT_AUDIO_EXTRA));
       intent.putExtra(ConversationActivity.DRAFT_VIDEO_EXTRA, getIntent().getParcelableExtra(ConversationActivity.DRAFT_VIDEO_EXTRA));
       intent.putExtra(ConversationActivity.DRAFT_IMAGE_EXTRA, getIntent().getParcelableExtra(ConversationActivity.DRAFT_IMAGE_EXTRA));
+
       long existingThread = DatabaseFactory.getThreadDatabase(this).getThreadIdIfExistsFor(recipients);
+
       intent.putExtra(ConversationActivity.THREAD_ID_EXTRA, existingThread);
       intent.putExtra(ConversationActivity.DISTRIBUTION_TYPE_EXTRA, ThreadDatabase.DistributionTypes.DEFAULT);
       startActivity(intent);
       finish();
     }
   }
+
 }
