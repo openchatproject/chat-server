0bcbb6251b6be53aff65eec7f2f18ff6ed528203
captionkirk <captionkirk@outlook.com>
2019-06-07 19:52:46 +0800

    fix issue with camera controls going missing

--- app/src/main/java/com/openchat/secureim/util/ViewUtil.java
+++ app/src/main/java/com/openchat/secureim/util/ViewUtil.java
@@ -3,6 +3,7 @@ package com.openchat.secureim.util;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.DrawableRes;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class ViewUtil {
   public static void setBackgroundSavingPadding(View v, Drawable drawable) {
@@ -21,5 +22,11 @@ public class ViewUtil {
     final int paddingTop = v.getPaddingTop();
     v.setBackgroundResource(resId);
     v.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
+  }
+
+  public static void swapChildInPlace(ViewGroup parent, View toRemove, View toAdd, int defaultIndex) {
+    int childIndex = parent.indexOfChild(toRemove);
+    if (childIndex > -1) parent.removeView(toRemove);
+    parent.addView(toAdd, childIndex > -1 ? childIndex : defaultIndex);
   }
 }
