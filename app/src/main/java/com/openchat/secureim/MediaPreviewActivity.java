c43ff022e248965469fba231c65119225ad6718c
TyroneFrancis <tyrone.fran@hotmail.com>
2019-06-17 13:06:14 +0800

    Maintenance: Removed unused field in MediaPreviewActivity

--- app/src/main/java/com/openchat/secureim/MediaPreviewActivity.java
+++ app/src/main/java/com/openchat/secureim/MediaPreviewActivity.java
@@ -3,7 +3,6 @@ package com.openchat.secureim;
 import android.annotation.TargetApi;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
@@ -37,7 +36,6 @@ public class MediaPreviewActivity extends PassphraseRequiredActionBarActivity im
 
   private MasterSecret masterSecret;
 
-  private Bitmap            bitmap;
   private ZoomingImageView  image;
   private Uri               mediaUri;
   private String            mediaType;
@@ -147,10 +145,6 @@ public class MediaPreviewActivity extends PassphraseRequiredActionBarActivity im
 
   private void cleanupMedia() {
     image.setImageDrawable(null);
-    if (bitmap != null) {
-      bitmap.recycle();
-      bitmap = null;
-    }
   }
 
   private void saveToDisk() {
